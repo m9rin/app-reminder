e5a33b92bef289583b2a90c0f74bb6c22524cfc7
Gabriel Marin <marinjhow@gmail.com>
2026-06-14 15:43:15 -0300

    feat: creates dto for users, and add bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async register(registerDto: RegisterDto) {
+    const userAlreadyExists = this.usersService.findByEmail(registerDto.email);
+
+    if (userAlreadyExists) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const user = this.usersService.create({ ...registerDto, password: hashedPassword });
+
+    return {
+      message: 'User registered successfully',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [];
+    private nextId: number = 1;
+
+    create(data: Omit<User, 'id'>) {
+        const user: User = {
+            id: this.nextId,
+            ...data
+        };
+        this.users.push(user);
+        this.nextId++;
+        return user;
+    }
+
+    findByEmail(email: string) {
+        return this.users.find(user => user.email === email);
+    }
+}
